39743306bedcf1dec0f35527c2ab2445d54e8936
Julie <Julie@HOME-PC>
2024-12-15 21:06:10 +0300

    Add project files.

---
+++ Les2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Les2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1();
+            //Task2();
+            //Task3();
+            Task4();
+        }
+
+        static void Task1()
+        {
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "notepad.exe",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(processInfo);
+            process.WaitForExit();
+            Console.WriteLine("Програма закрита");
+        }
+
+        static void Task2()
+        {
+            Console.WriteLine("\nВведіть назву програми для запуску:");
+            string processName = Console.ReadLine();
+
+            Process process = Process.Start(processName);
+
+            Console.WriteLine("\n1 - Чекати завершення");
+            Console.WriteLine("2 - Примусово завершити");
+            Console.WriteLine("\nВведіть цифру обраного варінату : ");
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    process.WaitForExit();
+                    Console.WriteLine("Процес завершено");
+                    break;
+                case "2":
+                    process.Kill();
+                    Console.WriteLine("Процес примусово завершено");
+                    break;
+                default:
+                    Console.WriteLine("Невірний вибір");
+                    break;
+            }
+
+        }
+
+        static void Task3()
+        {
+            Console.WriteLine("\nВведіть перше число:");
+            string num1 = Console.ReadLine();
+
+            Console.WriteLine("Введіть друге число:");
+            string num2 = Console.ReadLine();
+
+            Console.WriteLine("Введіть дію над числами:");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine();
+
+            string command = $"set /a result={num1}{operation}{num2}";
+            string filePath = "result.txt";
+
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C {command} > \"{filePath}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(processInfo);
+            process.WaitForExit();
+
+            string output = process.StandardOutput.ReadToEnd();
+            Console.WriteLine($"Результат записано в файл: {filePath}");
+            Process.Start("notepad.exe", filePath);
+        }
+
+        static void Task4()
+        {
+            Console.WriteLine("\nВведіть шлях до файлу:");
+            string filePath = Console.ReadLine();
+
+            Console.WriteLine("Введіть слово для пошуку:");
+            string word = Console.ReadLine();
+
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("Файл не знайдено");
+                return;
+            }
+
+            var processInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                Arguments = $"/C findstr /I /C:\"{word}\" \"{filePath}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            var process = Process.Start(processInfo);
+            string output = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            string fileContent = File.ReadAllText(filePath);
+            int count = CountWord(fileContent, word);
+
+            Console.WriteLine($"Слово '{word}' зустрічається {count} разів");
+        }
+
+        static int CountWord(string text, string word)
+        {
+            int count = 0;
+            int index = 0;
+
+            while ((index = text.IndexOf(word, index)) != -1)
+            {
+                count++;
+                index += word.Length;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ Les2/lesson.cs
@@ -0,0 +1,168 @@
+﻿//using System.Diagnostics;
+
+//namespace Les2
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            //string tempFile = Path.Combine(Path.GetTempPath(), "test.txt");
+//            ////File.Create(tempFile);
+
+//            //File.WriteAllText(tempFile, "Hello Notepad");
+
+//            //Process.Start("notepad.exe", tempFile);
+//            //Process.Start("notepad.exe");
+
+//            //var process = Process.GetProcessesByName("notepad");
+
+//            //Thread.Sleep(5000);
+
+//            //foreach(var item in process)
+//            //{
+//            //    //Console.WriteLine(item.StartInfo.FileName);
+//            //    item.Kill();
+//            //    item.WaitForExit();
+//            //    Console.WriteLine($"{item.ProcessName}");
+//            //}
+
+//            //Process.Start("notepad.exe");
+//            //Process.Start("notepad.exe", "text.txt");
+//            //Process.Start("notepad.exe", tempFile);
+
+//            /*----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//            //var processes = Process.GetProcesses();
+//            //foreach (var item in processes)
+//            //{
+//            //    Console.WriteLine($"Id - {item.Id}, Name - {item.ProcessName}");  
+//            //}
+
+
+//            //var processes = Process.GetProcesses().ToList().OrderBy(x => x.ProcessName);
+//            //foreach (var item in processes)
+//            //{
+//            //    Console.WriteLine($"Id - {item.Id}, Name - {item.ProcessName}");
+//            //}
+
+
+//            //var processes = Process.GetProcesses().ToList();
+//            //processes.Sort((p1, p2) => p1.ProcessName.CompareTo(p2.ProcessName)); //сортування через колекцію
+//            //foreach (var item in processes)
+//            //{
+//            //    Console.WriteLine($"Id - {item.Id}, Name - {item.ProcessName}");
+//            //}
+
+
+//            //var processes = Process.GetProcesses()
+//            //                       .OrderBy(x => x.ProcessName)
+//            //                       .ToList();
+
+//            //foreach (var item in processes)
+//            //{
+//            //    Console.WriteLine($"Id - {item.Id}, Name - {item.ProcessName}");
+//            //}
+
+//            /*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+//            //var processInfo = new ProcessStartInfo
+//            //{
+//            //    FileName = "ping",
+//            //    Arguments = "google.com",
+//            //    RedirectStandardOutput = true,
+//            //    UseShellExecute = false,
+//            //    CreateNoWindow = true,
+//            //};
+
+//            /////Process.Start(process);
+
+//            //var process = Process.Start(processInfo);
+//            //string output = process.StandardOutput.ReadToEnd();
+
+//            //process.WaitForExit();
+//            //Console.WriteLine(output);
+
+//            /*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//            //var processInfo = new ProcessStartInfo
+//            //{
+//            //    FileName = "cmd.exe",
+//            //    RedirectStandardOutput = true,
+//            //    RedirectStandardInput = true,
+//            //    UseShellExecute = false,
+//            //    CreateNoWindow = true,
+//            //};
+
+
+//            //var process = Process.Start(processInfo);
+
+//            //process.StandardInput.WriteLine("echo Hello from C#");
+//            //process.StandardInput.WriteLine("exit");
+
+//            //string output = process.StandardOutput.ReadToEnd();
+
+//            //process.WaitForExit();
+//            //Console.WriteLine(output);
+
+//            /*------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+//            //var process = new Process();
+//            //process.StartInfo.FileName = "notepad.exe";
+//            //process.EnableRaisingEvents = true;
+
+//            //process.Exited += (sender, e) =>
+//            //{
+//            //    //Console.WriteLine($"Sender - {sender.ToString}, Event - {e.ToString}");
+//            //    Console.WriteLine("Program exited");
+//            //};
+
+//            //process.Start();
+
+//            //for (int i = 0; i < 30; i++) 
+//            //{ 
+//            //    Thread.Sleep(1000);
+//            //    Console.WriteLine(i);
+//            //    if (i == 10)
+//            //    {
+//            //        process.Kill();
+//            //    }
+//            //}
+
+//            //process.WaitForExit();
+//            //Console.WriteLine("1111111111111111111");
+
+              /*-----------------------------------------------------------------------------*/
+
+            //string choice = Console.ReadLine();
+
+            //var processInfo = new ProcessStartInfo
+            //{
+            //    FileName = "notepad.exe",
+            //    RedirectStandardOutput = true,
+            //    UseShellExecute = false,
+            //    CreateNoWindow = true
+            //};
+
+            //Console.WriteLine("\n1 Чекати завершення");
+            //Console.WriteLine("2 Примусово завершити");
+            //Console.WriteLine("\nведіть цифру обраного варінату : ");
+
+            //var process = Process.Start(processInfo);
+
+            //switch (choice)
+            //{
+            //    case "1":
+            //        process.WaitForExit();
+            //        Console.WriteLine($"Процес завершено");
+            //        Console.WriteLine($"Код завершення: {process.ExitCode}");
+            //        break;
+            //    case "2":
+            //        process.Kill();
+            //        Console.WriteLine("Процес примусово завершено");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Невірний вибір");
+            //        break;
+            //}
+//        }
+//    }
+//}
